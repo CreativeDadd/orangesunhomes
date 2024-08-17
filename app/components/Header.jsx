b8df353726f915cf1a40de40b6c8b4ccb2e42e29
CreativeDad <oyebolang@gmail.com>
2024-08-18 00:14:22 +0100

    My Base code

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -10,10 +10,13 @@ export default function Header() {
   };
 
   return (
-    <nav className="bg-white shadow-md animated-header">
+    <nav className="bg-white shadow-md animated-header fixed top-0 w-full z-50">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         <Link href="/" legacyBehavior>
-          <a className="text-2xl font-bold text-orange-600">Orange-Sun Homes</a>
+          <a className="text-2xl font-bold text-orange-600">
+            <img src="/assets/images/logo.svg" alt="Corporate-logo"  width={50} height={50} />
+          </a>
+          
         </Link>
         <div className="hidden md:flex space-x-6 items-center">
           <Link href="/" legacyBehavior>
